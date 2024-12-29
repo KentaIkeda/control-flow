d4559cc3149cd13fa613d4627b11952d5bf53b4e
Kenta Ikeda <77259023+Krudaovo@users.noreply.github.com>
2024-12-29 14:04:29 +0900

    Implement conditional checks and divisibility logic in main function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,42 @@
 fn main() {
-    println!("Hello, world!");
+    let n = 3;
+
+    // Rustのif文は、式を（）で囲まず、そのまま記述する
+    if n < 5 {
+        println!("condition was true");
+    } else {
+        println!("condition was false");
+    }
+
+    // Rustのif文の式は、完全にboolであるべき
+    // JavaScriptの様に、暗黙的に値がboolに変換されることはない。
+    // このコードは、nが3ですが、trueと評価されず、コンパイルエラーが発生する
+    // if n {
+    //     println!("{}", n);
+    // }
+
+    {
+        const N: i32 = 6;
+
+        const IS_NOT_DIVISIBLE_2: bool = N % 2 != 0;
+        const IS_NOT_DIVISIBLE_3: bool = N % 3 != 0;
+        const IS_NOT_DIVISIBLE_4: bool = N % 4 != 0;
+
+        if IS_NOT_DIVISIBLE_4 && IS_NOT_DIVISIBLE_3  && IS_NOT_DIVISIBLE_2  {
+            println!("number is not divisible by 4, 3, or 2");
+            // 本来はエラーをスローさせたほうが良い
+            return;
+        }
+
+        if !IS_NOT_DIVISIBLE_4 {
+            // 数値は4で割り切れます
+            println!("number is divisible by 4");
+        } else if !IS_NOT_DIVISIBLE_3 {
+            // 数値は3で割り切れます
+            println!("number is divisible by 3");
+        } else if !IS_NOT_DIVISIBLE_2 {
+            // 数値は2で割り切れます
+            println!("number is divisible by 2");
+        }
+    }
 }
